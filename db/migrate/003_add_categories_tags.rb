9a8bb6de98663113e9ac1618bd4830a85f7f779f
dirkkelly <dirk.kelly@squaretalent.com>
2010-03-09 23:52:57 +0800

    clearing out old code

--- db/migrate/003_add_categories_tags.rb
+++
@@ -1,9 +0,0 @@
-class AddCategoriesTags < ActiveRecord::Migration
-	def self.up
-		add_column :categories, :tags, :string
-	end
-
-	def self.down
-		remove_column :categories, :tags
-	end
-end
--- db/migrate/20090922003900_add_product_images_tags.rb
+++
@@ -1,9 +0,0 @@
-class AddProductImagesTags < ActiveRecord::Migration
-	def self.up
-		add_column :product_images, :tags, :string
-	end
-
-	def self.down
-		remove_column :product_images, :tags
-	end
-end
