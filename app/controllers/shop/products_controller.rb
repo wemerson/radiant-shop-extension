04ab56d5e3767dbae53054134780a3a27f43afd5
dirkkelly <dirk.kelly@squaretalent.com>
2010-03-15 15:28:17 +0800

    removing attachment fu and working on acts_as_list

--- app/controllers/shop/products_controller.rb
+++ app/controllers/shop/products_controller.rb
@@ -1,10 +1,10 @@
 class Shop::ProductsController < ApplicationController
-	radiant_layout 'Product'
+  radiant_layout 'Product'
   no_login_required
 
-	def show
-	  @shop_product=ShopProduct.find(:first, {:conditions => ['LOWER(title) = ?', params[:title].downcase.gsub('_', ' ')], :include => :category})
-		@title = @shop_product.title
-		@radiant_layout=@shop_product.layout
-	end
+  def show
+    @shop_product=ShopProduct.find(:first, {:conditions => ['LOWER(title) = ?', params[:title].downcase.gsub('_', ' ')], :include => :category})
+    @title = @shop_product.title
+    @radiant_layout=@shop_product.layout
+  end
 end
