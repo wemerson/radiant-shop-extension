3160ae1b3643ed3753c0ef543a5f728645533e6f
John Barker <jebarker@gmail.com>
2010-08-27 16:09:32 +0800

    Move routes into config/routes.rb

--- shop_cart_extension.rb
+++ shop_cart_extension.rb
@@ -8,19 +8,6 @@ class ShopCartExtension < Radiant::Extension
   description "Describe your extension here"
   url "http://github.com/crankin/radiant-shop_cart-extension"
 
-  define_routes do |map|
-    map.namespace 'shop' do |shop|
-      shop.cart 'cart', :controller => 'orders', :action => 'show', :id => 'current'
-      shop.cart_items 'cart/items.:format', :controller => 'line_items', :action => 'index', :order_id => 'current', :conditions => { :method => :get } 
-      shop.cart_item_add 'cart/items.:format', :controller => 'line_items', :action => 'create', :conditions => { :method => :post } 
-      shop.cart_item_update 'cart/items/:id.:format', :controller => 'line_items', :action => 'update', :order_id => 'current', :conditions => { :method => :put } 
-      shop.cart_item_remove 'cart/items/:id/remove.:format', :controller => 'line_items', :action => 'destroy', :order_id => 'current'
-      shop.resources :orders do |orders|
-        orders.resources :line_items
-      end
-    end
-  end
-  
   extension_config do |config|
     config.gem 'activemerchant', :version => '~> 1.5.1', :lib => 'active_merchant', :source => 'http://gemcutter.org'
   end
