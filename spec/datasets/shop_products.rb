e66281c4843040eaaf0583e234eb725760a5e297
John Barker <jebarker@gmail.com>
2010-08-27 16:45:38 +0800

    Add some weight to the products

--- spec/datasets/shop_products.rb
+++ spec/datasets/shop_products.rb
@@ -14,8 +14,9 @@ class ShopProductsDataset < Dataset::Base
         create_record :shop_product, "#{product.to_s}_#{category.to_s}".to_sym,
           :name     => "#{product.to_s} #{category.to_s}",
           :sku      => "#{product.to_s}_#{category.to_s}",
-          :price    => i+1*10,
-          :position => i+1,
+          :price    => i + 1 * 10,
+          :position => i + 1,
+          :weight   => i + 10 * 3,
           :category => shop_categories(category)
       end
     end
