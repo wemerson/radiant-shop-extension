267ae04d90e42ac5584a5c5314cc515d67f78447
John Barker <jebarker@gmail.com>
2010-08-30 15:32:46 +0800

    Use rr for mocking

--- spec/controllers/shop/shop_categories_controller_spec.rb
+++ spec/controllers/shop/shop_categories_controller_spec.rb
@@ -8,44 +8,44 @@ describe Shop::CategoriesController do
     @shop_categories = [shop_categories(:bread), shop_categories(:salad)]
   end
   
-  describe "index" do    
-    it "should expose categories list" do
-      ShopCategory.stub!(:search).and_return(@shop_categories)
+  describe 'index' do
+    it 'should expose categories list' do
+      mock(ShopCategory).search(nil).returns(@shop_categories)
       get :index
       
       response.should be_success
     end
     
-    it "should return 404 if categories empty" do
-      ShopCategory.stub!(:search).and_return([])
+    it 'should return 404 if categories empty' do
+      mock(ShopCategory).search(nil).returns([])
       get :index
       
       response.should render_template('site/not_found')
     end
   end
   
-  describe "#show" do
-    it "should expose category" do
-      ShopCategory.stub!(:find).and_return(@shop_category)
+  describe '#show' do
+    it 'should expose category' do
+      mock(ShopCategory).find.returns(@shop_category)
       get :show
       
       response.should be_success
     end
     
-    it "should return 404 if product empty" do
-      ShopCategory.stub!(:find).and_return(nil)
+    it 'should return 404 if product empty' do
+      mock(ShopCategory).find
       get :show
       
       response.should render_template('site/not_found')
     end
     
-    it "should find a category by handle" do
+    it 'should find a category by handle' do
       get :show, :handle => @shop_category.handle
       
       response.should be_success
     end
     
-    it "should not find a category with an invalid handle" do
+    it 'should not find a category with an invalid handle' do
       get :show, :handle => 'i-wont-exist'
       
       response.should render_template('site/not_found')
