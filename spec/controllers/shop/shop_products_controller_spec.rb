f092473629f8374cdc5d0f483e084aa34bdad2e5
John Barker <jebarker@gmail.com>
2010-08-30 15:56:17 +0800

    Use rr for mocking

--- spec/controllers/shop/shop_products_controller_spec.rb
+++ spec/controllers/shop/shop_products_controller_spec.rb
@@ -4,48 +4,48 @@ describe Shop::ProductsController do
   dataset :shop_products
   
   before(:each) do
-    @shop_product = shop_products(:white_bread)
-    @shop_products = [shop_products(:white_bread), shop_products(:wholemeal_bread), shop_products(:multigrain_bread)]
+    @shop_product = shop_products(:soft_bread)
+    @shop_products = [shop_products(:soft_bread), shop_products(:crusty_bread), shop_products(:warm_bread)]
   end
   
-  describe "index" do    
-    it "should expose products list" do
-      ShopProduct.stub!(:search).and_return(@shop_products)
+  describe 'index' do
+    it 'should expose products list' do
+      mock(ShopProduct).search(nil).returns(@shop_products)
       get :index
       
       response.should be_success
     end
     
-    it "should return 404 if products empty" do
-      ShopProduct.stub!(:search).and_return([])
+    it 'should return 404 if products empty' do
+      mock(ShopProduct).search(nil).returns([])
       get :index
       
       response.should render_template('site/not_found')
     end
   end
   
-  describe "#show" do
-    it "should expose product" do
-      ShopProduct.stub!(:find).and_return(@shop_product)
+  describe '#show' do
+    it 'should expose product' do
+      mock(ShopProduct).find.returns(@shop_product)
       get :show
       
       response.should be_success
     end
     
-    it "should return 404 if product empty" do
-      ShopProduct.stub!(:find).and_return(nil)
+    it 'should return 404 if product empty' do
+      mock(ShopProduct).find.returns(nil)
       get :show
       
       response.should render_template('site/not_found')
     end
     
-    it "should find a product by handle" do
+    it 'should find a product by handle' do
       get :show, :handle => @shop_product.handle
       
       response.should be_success
     end
     
-    it "should not find a product with an invalid handle" do
+    it 'should not find a product with an invalid handle' do
       get :show, :handle => 'i-wont-exist'
       
       response.should render_template('site/not_found')
