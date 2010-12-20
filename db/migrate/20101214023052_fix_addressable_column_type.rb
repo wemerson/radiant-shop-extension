f1d584b856f3990d458405d64a1503ea30b90658
Dirk Kelly <dk@dirkkelly.com>
2010-12-20 17:35:33 +0800

    rescue column migration breaking in sqlite

--- db/migrate/20101214023052_fix_addressable_column_type.rb
+++ db/migrate/20101214023052_fix_addressable_column_type.rb
@@ -5,13 +5,20 @@ class FixAddressableColumnType < ActiveRecord::Migration
     ShopAddress.find_each do |address|
       address.update_attribute(:addressable_integer, :addressable_id)
     end
-    remove_column :shop_addresses, :addressable_id
     
-    add_column :shop_addresses, :addressable_id, :integer
-    ShopAddress.reset_column_information
-    ShopAddress.find_each do |address|
-      address.update_attribute(:addressable_id, :addressable_integer)
-    end  
+    begin
+      remove_column :shop_addresses, :addressable_id
+      add_column :shop_addresses, :addressable_id, :integer
+      ShopAddress.reset_column_information
+      ShopAddress.find_each do |address|
+        address.update_attribute(:addressable_id, :addressable_integer)
+      end  
+      
+      remove_column :shop_addresses, :addressable_integer
+    rescue
+      # Yay sqlite
+    end
+    
   end
 
   def self.down
